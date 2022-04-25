a39ced8e09513695623a5232012ab3f1700115e9
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-04-25 15:28:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const prompt = require(`prompt-sync`)();
 const alternativas = ["tesoura", "papel", "pedra"];
 // Variaveis
 let rodadas = 0;
-let cont = 0;
+let cont = 1;
 let vitoria = 0;
 let derrota = 0;
-let empate = 0;
+let empate = 0;''
 let jogador = "";
-// Validação de quantas jogadas iremos jogar 
+// Validação de quantas jogadas iremos jogar
 primeiro: while (true) {
   rodadas = +prompt(`Quantas partidas iremos jogar? `);
   if (rodadas > 0) {
@@ -16,41 +16,40 @@ primeiro: while (true) {
     console.log("ERRO! Entrada invalida - Digite um NUMERO maior que 0");
     continue primeiro;
   }
-// Game Start
-  segundo: while (cont <= rodadas - 1) {
-    cont++;
-// Validação da escolha do jogador
-    terceiro: while (true) {
-      jogador = prompt(`Qual sua jogada [pedra, papel ou tesoura]? `).toLowerCase();
+  // Game Start
+  for (cont; cont <= rodadas ; cont++) {
+    // Validação da escolha do jogador
+    segundo: while (true) {
+      jogador = prompt(
+        `Qual sua jogada [pedra, papel ou tesoura]? `
+      ).toLowerCase();
       if (jogador === "pedra" || jogador === "papel" || jogador == "tesoura") {
-        break terceiro;
+        break segundo;
       } else {
         console.log("ERRO! Digite uma entrada valida [pedra, papel, tesoura]");
-        continue terceiro;
+        continue segundo;
       }
     }
-// Jogada do computador + verificando quem ganhou a rodada
+    // Jogada do computador + verificando quem ganhou a rodada
     let computador = Math.floor(Math.random() * 3);
-    for (i = 1; i <= 1; i++) {
-      let pc = alternativas[computador];
-      console.log(`JOGADOR: ${jogador}`);
-      console.log(`COMPUTADOR: ${pc}`);
-      if (jogador === pc) {
-        empate++;
-        console.log("EMPATE");
-      } else if (
-        (jogador === `pedra` && pc === `papel`) ||
-        (jogador === `papel` && pc === `tesoura`) ||
-        (jogador === `tesoura` && pc === `pedra`)
-      ) {
-        derrota++;
-        console.log("DERROTA");
-      } else {
-        vitoria++;
-        console.log("VITORIA");
-      }
+    let pc = alternativas[computador];
+    console.log(`JOGADOR: ${jogador}`);
+    console.log(`COMPUTADOR: ${pc}`);
+    if (jogador === pc) {
+      empate++;
+      console.log("EMPATE");
+    } else if (
+      (jogador === `pedra` && pc === `papel`) ||
+      (jogador === `papel` && pc === `tesoura`) ||
+      (jogador === `tesoura` && pc === `pedra`)
+    ) {
+      derrota++;
+      console.log("DERROTA");
+    } else {
+      vitoria++;
+      console.log("VITORIA");
     }
-// Dados estatisticos de todas as partidas    
+    // Dados estatisticos de todas as partidas
     if (cont === rodadas) {
       console.log(`                   → DADOS ESTATISTICOS ←
       [PARTIDAS = ${rodadas} ]  [JOGADOR = ${vitoria}]  [COMPUTADOR = ${derrota}]  [EMPATE = ${empate}]`);
@@ -61,13 +60,13 @@ primeiro: while (true) {
       } else {
         console.log(`\n  [→→ O JOGO EMPATOU ←←]`);
       }
-// Validação da escolha se o jogador quer continuar ou não jogando.     
+      // Validação da escolha se o jogador quer continuar ou não jogando.
       quarto: while (true) {
         let jogadorDenovo = prompt(
           `Deseja jogar denovo [s] para SIM [n] para NÃO: `
         ).toLowerCase();
         if (jogadorDenovo === "s") {
-          cont = 0;
+          cont = 1;
           vitoria = 0;
           derrota = 0;
           empate = 0;
